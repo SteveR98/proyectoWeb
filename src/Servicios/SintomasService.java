0158bc8c3b0e0cb1035057331122da1e22d041db
SteveR98 <alvarodava@gmail.com>
2017-02-28 18:15:18 +0100

    servlet update

--- src/Servicios/SintomasService.java
+++ src/Servicios/SintomasService.java
@@ -13,28 +13,35 @@ public class SintomasService {
 		List<SintomasDTO> lista_descripcion = new ArrayList<SintomasDTO>();
 		List<SintomasDTO> lista_sintomas = ListadoSintomas.listaSintomasCompleta();
 		String sintoma_descripcion = null;
-		
+		String[] palabras_sintoma_descripcion ;
+				
 		inicial = inicial.toLowerCase();
 		
 		for (SintomasDTO sintoma:lista_sintomas)
 		{
 			sintoma_descripcion = sintoma.getDescripcion();
 	        
-	        if(sintoma_descripcion.startsWith(inicial)) 
+	        palabras_sintoma_descripcion = separarFrase(sintoma_descripcion);
+	        
+	        for(String palabra_sintoma:palabras_sintoma_descripcion)
 	        {
-	        	lista_descripcion.add(sintoma);
+	        	if(palabra_sintoma.startsWith(inicial)) 
+	        	{
+	        		lista_descripcion.add(sintoma);
+	        	}
 	        }
 		}
 		
 		return lista_descripcion;
 	}
 	
-	public static void imprimirLista(List<String> lista_descripcion)
-	{
-		for(String descripcion:lista_descripcion)
-		{
-			System.out.println(descripcion);
-		}
-	}
 	
+	
+    public static String[] separarFrase(String frase_introducida) {
+
+    	String delimitadores= "[ .,;?!¡¿\'\"\\[\\]]+";
+    	String[] palabrasSeparadas = frase_introducida.split(delimitadores);
+    	
+        return palabrasSeparadas;
+    }
 }
